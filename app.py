c78fea4ac149af482a8ef236cc6786c306858fc5
aboud20-meet <aboud20@meet.mit.edu>
2018-10-25 18:47:00 +0300

    wowowo

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import *
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return None
+   	return render_template("home.html")
 
+@app.route('/shop')
+def shop_page():
+	items = ['MAXI FLORAL DRESS', 'DENIM JACKET', 'FAKE CHANEL PURSE']
+	return render_template(
+		"shop.html",
+		items=items)
+
+	
 if __name__ == '__main__':
    app.run(debug = True)
